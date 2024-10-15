56d7feb12df038b054d8a078d201b6d6452ca86f
xungyun <syoon0309@naver.com>
2024-10-15 22:03:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#searchBooks').on('click', function() {
+      alert('도서검색 기능이 구현되었습니다.');
+  });
+
+  $('#loanStatus').on('click', function() {
+      alert('대출 현황 기능이 구현되었습니다.');
+  });
+
+  $('#returnBooks').on('click', function() {
+      alert('도서 반납 기능이 구현되었습니다.');
+  });
+
+  $('#myAccount').on('click', function() {
+      alert('내 계정 기능이 구현되었습니다.');
+  });
+});
